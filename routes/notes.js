ad85c01776eefb0d434ca5f5825ea2849997da0d
rileysong01 <rsong47@uwo.ca>
2023-09-10 17:18:10 -0400

    added delete note feature

--- routes/notes.js
+++ routes/notes.js
@@ -20,13 +20,26 @@ notes.post('/', (req, res) => {
         const newNote = {
             title,
             text,
-            tip_id: uuidv4()
+            id: uuidv4()
         }
         readAndAppend(newNote, 'db/notes.json')
     } else {
         res.status(500).json({ error: 'Error in adding tip' });
     }
-
 });
+
+// delte route for specific tip
+notes.delete('/:id', (req, res) => {
+    console.log(req)
+    const noteId = req.params.id;
+    readFromFile('db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const newArray = json.filter((note) => note.id !== noteId)
+        writeToFile('db/notes.json', newArray)
+        console.log(`item ${noteId} has been deleted`)
+        res.json('item has been deleted')
+    })
+})
 
 module.exports = notes;
